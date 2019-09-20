915eb3f18fc7e56e86ed73773413c445be049be2
Boris Schrijver <bschrijver@schubergphilis.com>
2019-09-20 11:10:23 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,115 @@
+locals {
+  datadog_integration_role_name = "DatadogAWSIntegrationRole"
+  datadog_aws_account_id        = "464622532012"
+}
+
+provider "datadog" {}
+
+data "aws_caller_identity" "current" {}
+
+resource "datadog_integration_aws" "default" {
+  account_id = data.aws_caller_identity.current.account_id
+  role_name  = local.datadog_integration_role_name
+}
+
+
+data "aws_iam_policy_document" "datadog_integration_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.datadog_aws_account_id}:root"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+
+      values = [
+        datadog_integration_aws.default.external_id
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "datadog_integration_policy" {
+  statement {
+    actions = [
+      "apigateway:GET",
+      "autoscaling:Describe*",
+      "budgets:ViewBudget",
+      "cloudfront:GetDistributionConfig",
+      "cloudfront:ListDistributions",
+      "cloudtrail:DescribeTrails",
+      "cloudtrail:GetTrailStatus",
+      "cloudwatch:Describe*",
+      "cloudwatch:Get*",
+      "cloudwatch:List*",
+      "codedeploy:List*",
+      "codedeploy:BatchGet*",
+      "directconnect:Describe*",
+      "dynamodb:List*",
+      "dynamodb:Describe*",
+      "ec2:Describe*",
+      "ecs:Describe*",
+      "ecs:List*",
+      "elasticache:Describe*",
+      "elasticache:List*",
+      "elasticfilesystem:DescribeFileSystems",
+      "elasticfilesystem:DescribeTags",
+      "elasticloadbalancing:Describe*",
+      "elasticmapreduce:List*",
+      "elasticmapreduce:Describe*",
+      "es:ListTags",
+      "es:ListDomainNames",
+      "es:DescribeElasticsearchDomains",
+      "health:DescribeEvents",
+      "health:DescribeEventDetails",
+      "health:DescribeAffectedEntities",
+      "kinesis:List*",
+      "kinesis:Describe*",
+      "lambda:AddPermission",
+      "lambda:GetPolicy",
+      "lambda:List*",
+      "lambda:RemovePermission",
+      "logs:Get*",
+      "logs:Describe*",
+      "logs:FilterLogEvents",
+      "logs:TestMetricFilter",
+      "logs:PutSubscriptionFilter",
+      "logs:DeleteSubscriptionFilter",
+      "logs:DescribeSubscriptionFilters",
+      "rds:Describe*",
+      "rds:List*",
+      "redshift:DescribeClusters",
+      "redshift:DescribeLoggingStatus",
+      "route53:List*",
+      "s3:GetBucketLogging",
+      "s3:GetBucketLocation",
+      "s3:GetBucketNotification",
+      "s3:GetBucketTagging",
+      "s3:ListAllMyBuckets",
+      "s3:PutBucketNotification",
+      "ses:Get*",
+      "sns:List*",
+      "sns:Publish",
+      "sqs:ListQueues",
+      "support:*",
+      "tag:GetResources",
+      "tag:GetTagKeys",
+      "tag:GetTagValues",
+      "xray:BatchGetTraces",
+      "xray:GetTraceSummaries"
+    ]
+    resources = ["*"]
+  }
+}
+
+module "datadog_integration_role" {
+  providers     = { aws = aws }
+  source        = "github.com/schubergphilis/terraform-aws-mcaf-role?ref=v0.1.5"
+  name          = local.datadog_integration_role_name
+  assume_policy = data.aws_iam_policy_document.datadog_integration_assume_role.json
+  role_policy   = data.aws_iam_policy_document.datadog_integration_policy.json
+  tags          = var.tags
+}
