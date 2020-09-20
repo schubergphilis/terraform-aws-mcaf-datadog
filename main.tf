d7e9dc92c9c3987e0870950c759152a0c0848070
Stephen Hoekstra <shoekstra@schubergphilis.com>
2020-09-20 10:31:55 +0200

    Install AWS Forwarder
    As per https://docs.datadoghq.com/serverless/forwarder/ the Datadog
    Forwarder is an AWS Lambda to ship logs, custom metrics and traces to a
    Datadog org. See page for more info.
    
    Bear in mind this just sets up the Lambda and does not configure any
    forwarding, that is done outside of this module at this time.
    
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 locals {
   datadog_integration_role_name = "DatadogAWSIntegrationRole"
   datadog_aws_account_id        = "464622532012"
+
+  install_log_forwarder = var.api_key != null && var.install_log_forwarder ? 1 : 0
 }
 
 data "aws_caller_identity" "current" {}
@@ -113,4 +115,47 @@ module "datadog_integration_role" {
   assume_policy = data.aws_iam_policy_document.datadog_integration_assume_role.json
   role_policy   = data.aws_iam_policy_document.datadog_integration_policy.json
   tags          = var.tags
+}
+
+resource "aws_secretsmanager_secret" "api_key" {
+  count       = local.install_log_forwarder
+  name        = replace("${var.log_forwarder_name}_api_key", "-", "_")
+  description = "Datadog API key used by ${var.log_forwarder_name} lambda"
+}
+
+resource "aws_secretsmanager_secret_version" "api_key" {
+  count         = local.install_log_forwarder
+  secret_id     = aws_secretsmanager_secret.api_key.0.id
+  secret_string = var.api_key
+}
+
+resource "aws_cloudformation_stack" "datadog_forwarder" {
+  count        = local.install_log_forwarder
+  name         = var.log_forwarder_name
+  capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
+  on_failure   = "ROLLBACK"
+  template_url = "https://datadog-cloudformation-template.s3.amazonaws.com/aws/forwarder/${var.log_forwarder_version}.yaml"
+  tags         = var.tags
+
+  parameters = {
+    DdApiKey          = "this_value_is_not_used"
+    DdApiKeySecretArn = aws_secretsmanager_secret.api_key.0.arn
+    FunctionName      = var.log_forwarder_name
+  }
+
+  // The DdApiKey parameter has the NoEcho tag set in the cfn template, causing
+  // Terraform to reset the value, so we ignore it.
+  lifecycle {
+    ignore_changes = [
+      parameters["DdApiKey"]
+    ]
+  }
+
+  depends_on = [datadog_integration_aws.default]
+}
+
+resource "datadog_integration_aws_lambda_arn" "default" {
+  count      = local.install_log_forwarder
+  account_id = data.aws_caller_identity.current.account_id
+  lambda_arn = aws_cloudformation_stack.datadog_forwarder.0.outputs["DatadogForwarderArn"]
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,31 @@
+variable "api_key" {
+  type        = string
+  default     = null
+  description = "Datadog API key"
+}
+
 variable "datadog_tags" {
   type        = list(string)
   default     = []
   description = "Array of tags (in the form key:value) to add to all hosts and metrics"
+}
+
+variable "install_log_forwarder" {
+  type        = bool
+  default     = false
+  description = "Set to true to install the Datadog Log Forwarder (requires var.api_key to be set)"
+}
+
+variable "log_forwarder_name" {
+  type        = string
+  default     = "datadog-forwarder"
+  description = "AWS log forwarder lambda name"
+}
+
+variable "log_forwarder_version" {
+  type        = string
+  default     = "latest"
+  description = "AWS log forwarder version to install"
 }
 
 variable "tags" {
