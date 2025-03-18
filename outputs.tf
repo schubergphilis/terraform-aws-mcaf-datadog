98bfd9bf49102f7b7e14e4f43f48d81538c3b6cc
Martin Lefler <mlefler@schubergphilis.com>
2025-03-18 15:43:34 +0100

    output created api_key for when necessary

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+output "api_key" {
+  description = "Datadog API key if created by the module"
+  value       = var.create_api_key ? datadog_api_key.default[0].key : null
+  sensitive   = true
+}
+
 output "forwarder_arn" {
   description = "Datadog log forwarder lambda ARN"
   value       = var.install_log_forwarder ? aws_cloudformation_stack.datadog_forwarder[0].outputs["DatadogForwarderArn"] : ""
